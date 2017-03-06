3c3135fc3fe34297a27d53aa78308a80548284cc
pain <916459729@qq.com>
2017-03-06 21:15:39 +0800

    使用express写blog

--- app.js
+++ app.js
@@ -1,113 +1,35 @@
-var http = require('http');
+//采用路由
+
+var express = require('express');
 
 var fs = require('fs');
 
-var path = require('path');
+var app = express();
 
-//引入mime模块
-var mime = require('mime');
+//var router = express.Router();
 
-var xtpl = require('xtpl');
+//第一个参数是路径. 默认是 '/'
+app.use('/statics', express.static('statics'));
 
-var server = http.createServer();
+app.get('/', function (req, res) {
 
-function crrResponse(response) {
-    response.render = function (path, data) {
-        xtpl.renderFile(path, {
-            data: data
-        }, function (err, data) {
-            if(err){
-                response.writeHead(500);
-                response.end('服务器错误' + err);
-                return;
-            }
-
-            response.writeHead(200, {
-                'Content-type': 'text/html'
-            });
-
-            response.end(data);
-        });
-    }
-}
-
-server.on('request', function (request, response) {
-    crrResponse(response);
-
-    var url = request.url.toLowerCase();
-
-    if(url === '/' || url === '/views/index'){
-
-        response.render('./views/index.html', {});
-
-    }
-    else if(url === '/blog'){
-
-        fs.readFile('./data.json', 'utf8', function (err, data) {
-
-            var data = JSON.parse(data);
-
-            response.render('./views/post.html', data.reverse());
-
-        });
-    }
-    else if(url.startsWith('/submit')){
-
-        fs.readFile('./data.json', 'utf8', function (err, data) {
-
-            var data = JSON.parse(data);
-
-            data.push(parse(url));
-
-            fs.writeFile('./data.json', JSON.stringify(data));
-
-            response.render('./views/post.html', data.reverse());
-
-        });
-    }
-
-    else if(url === '/getcomment'){
-        fs.readFile('./data.json', 'utf8', function (err, data) {
-            response.end(data);
-        });
-    }
-    else if(url.startsWith('/statics')){
-        var filename = path.join(__dirname, url);
-
-        if(filename.indexOf('?') > 0){
-            filename = filename.substring(0, filename.indexOf('?'));
-        }
-
-        fs.readFile(filename, function (err, data) {
-            if(err){
-                response.writeHead(500);
-                response.end('服务器错误');
-                return;
-            }
-
-            response.writeHead(200, {
-                'Content-type': mime.lookup(filename)
-            });
-
-            response.end(data);
-        });
-
-    }
-    else {
-        response.end('找不到要请求的页面');
-    }
-});
-
-server.listen(2222, function (err) {
-    console.log('正在监听 2222');
-});
-
-function parse(url) {
-    var obj = {};
-    var arr = url.substring(url.indexOf('?') + 1).split('&');
-    arr.forEach(function (item) {
-        var ret = item.split('=');
-        obj[ret[0]] = ret[1];
+    fs.readFile('./views/index.html', 'utf8', function (err, data) {
+        res.send(data);
     });
-    return obj;
-}
+
+});
+
+
+app.get('/blog', function (req, res) {
+    fs.readFile('./views/post.html', 'utf8', function (err, data) {
+        res.send(data);
+    });
+});
+
+
+
+app.listen(2222, function (err) {
+    console.log('正在监听 2222 ');
+});
+
+
